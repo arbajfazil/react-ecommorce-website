0c312fc296161316f80068276a238e5db83a2683
arbajfazil <4arbajalam@gmail.com>
2026-07-15 18:11:19 +0530

    df

--- e-web/src/App.jsx
+++ e-web/src/App.jsx
@@ -1,6 +1,8 @@
 import { HomePage } from './pages/HomePage';
 import { Routes, Route } from "react-router-dom";
 import { CheckoutPage } from './pages/CheckoutPage';
+import { OrdersPage } from './pages/OrdersPage';
+import { TrackingPage } from './pages/TrackingPage';
 import './App.css';
 function App() {
 
@@ -8,7 +10,9 @@ function App() {
     <>
     <Routes>
       <Route index element={<HomePage />} />
-      <Route path="/checkout" element={<CheckoutPage />}/>
+      <Route path="/checkout" element={<CheckoutPage />} />
+      <Route path='/orders' element={<OrdersPage/>} />
+      <Route path='/TrackingPage' element={<TrackingPage />} />
     </Routes>
     </>
   )
