2b11e45e332da11dfaf4ea6b2b8f87ad99571fc7
arbajfazil <4arbajalam@gmail.com>
2026-07-20 11:04:11 +0530

    d

--- e-web/src/pages/HomePage.jsx
+++ e-web/src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import './HomePage.css'
+import { formatMoney } from '../utils/money';
 import axios from 'axios';
 import { useEffect,useState } from 'react';
 
@@ -41,7 +42,7 @@ export function HomePage({cart}){
                 </div>
 
                 <div className="product-price">
-                    {(product.priceCents /100).toFixed(2)}
+                    {formatMoney(product.priceCents)}
                 </div>
 
                 <div className="product-quantity-container">
